90668d5c0f812792cc22574361ef7cc12e18321b
demola234 <kolawoleoluwasegun567@gmail.com>
2025-03-07 07:40:12 +0100

    refactor: rename parameter 'value' to 'target' in binary search function for clarity

--- data-structures/arrary/binary_search.dart
+++ data-structures/arrary/binary_search.dart
@@ -1,15 +1,15 @@
-int binarySearch<T extends Comparable>(List<T> list, T value) {
+int binarySearch<T extends Comparable>(List<T> list, T target) {
   int left = 0;
   int right = list.length - 1;
 
   while (left <= right) {
     final mid = left + (right - left) ~/ 2;
 
-    if (list[mid] == value) {
+    if (list[mid] == target) {
       return mid;
     }
 
-    if (list[mid].compareTo(value) < 0) {
+    if (list[mid].compareTo(target) < 0) {
       left = mid + 1;
     } else {
       right = mid - 1;
